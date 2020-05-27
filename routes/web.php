facadfc2f321ea5bf971b2cbd37971f4043f0b4f
sub <subb.suanduang@gmail.com>
2020-05-27 16:35:15 +0700

    edit file

--- routes/web.php
+++ routes/web.php
@@ -15,17 +15,9 @@ Route::get('/', function () {
     return view('index');
 });
 
-<<<<<<< HEAD
-Route::resource('industrialEstate','IndustrialEstateController');
-// Route::resource('subb','SubbsController');
-=======
->>>>>>> e75085f54ea9a8c91e8e540e694d28604b62c3b4
 
 Auth::routes();
 
 Route::get('/dashboard', 'HomeController@index')->name('dashboard');
 
-<<<<<<< HEAD
-=======
 Route::resource('industrialEstate','IndustrialEstateController');
->>>>>>> e75085f54ea9a8c91e8e540e694d28604b62c3b4

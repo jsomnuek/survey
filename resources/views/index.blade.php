82029b74426359776f300ca28107acdae7154764
nititpong <nititpong_m@outlook.com>
2020-05-28 14:42:24 +0700

    Ok

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">Welcome To Lab Survey</div>
+                <div class="card-header">Welcome To Lab</div>
 
                 <div class="card-body">
                     @if (session('status'))

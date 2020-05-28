221d528642b8f59f55eb5371ec6fe42cd65671d3
sub <subb.suanduang@gmail.com>
2020-05-28 13:45:50 +0700

    create file migration technical equipment

---
+++ database/migrations/2020_05_28_103255_create_technical_equipment_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTechnicalEquipmentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('technical_equipment', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('technical_equipment_name', 255);
+            $table->string('technical_equipment_status' ,1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('technical_equipment');
+    }
+}

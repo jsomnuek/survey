a67528cdd7f0473dda6994550d774ede27f8801d
nititpong <nititpong_m@outlook.com>
2020-06-02 16:56:44 +0700

    edit file migration org

--- database/migrations/2020_06_02_095503_create_organizes_table.php
+++ database/migrations/2020_06_02_095503_create_organizes_table.php
@@ -15,6 +15,31 @@ class CreateOrganizesTable extends Migration
     {
         Schema::create('organizes', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('org_name',255);
+            $table->string('org_code',255);
+            $table->string('org_number',255)->nullable();
+            $table->string('org_location',255);
+            $table->string('org_building',255)->nullable();
+            $table->string('org_floor',255)->nullable();
+            $table->string('org_address',255);
+            $table->string('org_road',255)->nullable();
+            $table->string('org_soi',255)->nullable();
+            $table->string('org_postcode',6);
+            $table->integer('org_ch_id');        
+            $table->integer('org_am_id');        
+            $table->integer('org_ta_id');
+            $table->string('org_phone',255);   
+            $table->string('org_fax',255);   
+            $table->string('org_email',255);  
+            $table->string('org_website',255)->nullable();
+            $table->string('org_lat',255)->nullable();  
+            $table->string('org_long',255)->nullable();
+            $table->string('org_type',255)->nullable();
+            $table->string('org_business',255)->nullable();
+            $table->string('org_import_export',255)->nullable();
+            $table->string('org_capital',255)->nullable();
+            $table->string('org_employee',255)->nullable();
+            $table->string('org_industrial_type',255)->nullable();
             $table->timestamps();
         });
     }

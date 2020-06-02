fb9c45cecea356db102e08f28a4a822befc324b4
nititpong <nititpong_m@outlook.com>
2020-06-02 16:01:11 +0700

    edit views

--- resources/views/organize.blade.php
+++ resources/views/organize.blade.php
@@ -197,4 +197,83 @@
 </div>
 @endsection
 
+@section('js')
+<script>
+  $(document).ready(function() {
+
+      $.ajaxSetup({
+          headers: {
+              'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+          }
+      });
+
+      function showChangwats () {
+          $.ajax({
+              url: "{{ route('changwats') }}",
+              type: 'GET',
+              dataType: 'JSON',
+              success: function (data) {
+                  // console.log(data);
+                  for(let i = 0; i < data.length; i++){
+                      $("#changwats").append(`
+                          <option value="${data[i].ch_id}">${data[i].changwat_t}</option>
+                      `);
+                  }
+              }
+          });                
+      }
+      showChangwats();
+
+      function showAmphoeTambon() {
+          // Amphoe
+          $('#changwats').change(function (){
+              let ch_id = $(this).val();
+              $("#amphoes").html('<option value="" selected>select เขต/อำเภอ</option>');
+              if(ch_id != null) {
+                  $.ajax({
+                      type:'POST',
+                      url:"{{ route('amphoes.post') }}",
+                      data:{ch_id:ch_id},
+                      success:function(data){
+                          for(let i = 0; i < data.length; i++){
+                          $("#amphoes").append(`
+                              <option value="${data[i].am_id}">${data[i].amphoe_t}</option>
+                          `);
+                          }
+                      }
+                  });
+              } else {
+                  $("#amphoes").html();
+              }
+              
+          });
+          // Tambon
+          $('#amphoes').change(function (){
+              let am_id = $(this).val();
+              $("#tambons").html('<option value="" selected>select เขต/อำเภอ</option>');
+              if(am_id != null) {
+                  $.ajax({
+                      type:'POST',
+                      url:"{{ route('tambons.post') }}",
+                      data:{am_id:am_id},
+                      success:function(data){
+                          for(let i = 0; i < data.length; i++){
+                          $("#tambons").append(`
+                              <option value="${data[i].ta_id}">${data[i].tambon_t}</option>
+                          `);
+                          }
+                      }
+                  });
+              } else {
+                  $("#tambons").html();
+              }
+              
+          });
+      }
+      showAmphoeTambon();
+
+  });
+</script>
+@endsection
+
 

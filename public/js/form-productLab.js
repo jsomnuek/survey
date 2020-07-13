8d73e1cd6921b178ab32d865e16e65ae0bfdd9f3
sub <subb.suanduang@gmail.com>
2020-07-13 09:31:48 +0700

    update js after pull

--- public/js/form-productLab.js
+++ public/js/form-productLab.js
@@ -1,43 +1,4 @@
 $(document).ready(function() {
-<<<<<<< HEAD
-    //Initialize Select2 Elements
-    $(".select2")
-        .select2({
-            theme: "bootstrap4",
-            allowClear: true,
-            placeholder: "-- โปรดเลือก --"
-        })
-        .trigger("change");
-
-    //Initialize Select2 Elements
-    $(".select2-multi")
-        .select2({
-            theme: "bootstrap4",
-            allowClear: true,
-            placeholder: "-- โปรดเลือก --"
-        })
-        .trigger("change");
-
-    //4.2 checkProductTypes
-    function checkProductType() {
-        // this value
-        var data = $("#product_type_id").val();
-        console.log(data);
-        if (data != null) {
-            for (i = 0; i < data.length; i++) {
-                if (data[i] == "39") {
-                    var indTypeId = 39;
-                }
-            }
-        }
-        if (indTypeId == 39) {
-            $("#display_product_type_other").removeClass("d-none");
-            $("#product_type_other").prop("required", true);
-        } else {
-            $("#display_product_type_other").addClass("d-none");
-            $("#product_type_other").val(null);
-            $("#product_type_other").prop("required", false);
-=======
     // checkInput
     function checkInput() {
         let inputTypeNumber = [
@@ -57,33 +18,17 @@ $(document).ready(function() {
                     e.preventDefault();
                 }
             });
->>>>>>> 39774622bbfac40ad4a2c5028ed46d645a7942f6
         }
     }
     checkInput();
 
-<<<<<<< HEAD
-    //4.14 checkResultControl
-    function checkResultControl() {
-=======
     //4.2 ProductType
     function checkProductType() {
->>>>>>> 39774622bbfac40ad4a2c5028ed46d645a7942f6
         // this value
-        var data = $("#result_control_id").val();
+        var data = $("#product_type_id").val();
         console.log(data);
         if (data != null) {
             for (i = 0; i < data.length; i++) {
-<<<<<<< HEAD
-                if (data[i] == "7") {
-                    var indTypeId = 7;
-                }
-            }
-        }
-        if (indTypeId == 7) {
-            $("#display_result_control_other").removeClass("d-none");
-            $("#result_control_other").prop("required", true);
-=======
                 if (data[i] == "39") {
                     var productTypeId = 39;
                 }
@@ -92,57 +37,29 @@ $(document).ready(function() {
         if (productTypeId == 39) {
             $("#display_product_type_other").removeClass("d-none");
             $("#product_type_other").prop("required", true);
->>>>>>> 39774622bbfac40ad4a2c5028ed46d645a7942f6
         } else {
-            $("#display_result_control_other").addClass("d-none");
-            $("#result_control_other").val(null);
-            $("#result_control_other").prop("required", false);
+            $("#display_product_type_other").addClass("d-none");
+            $("#product_type_other").val(null);
+            $("#product_type_other").prop("required", false);
         }
         // event on selected
-<<<<<<< HEAD
-        $("#result_control_id").on(
-            "select2:select select2:unselect",
-            function() {
-                var data = $("#result_control_id").val();
-                if (data != null) {
-                    for (i = 0; i < data.length; i++) {
-                        if (data[i] == "7") {
-                            var indTypeId = 7;
-                        }
-=======
         $("#product_type_id").on("select2:select select2:unselect", function() {
             var data = $("#product_type_id").val();
             if (data != null) {
                 for (i = 0; i < data.length; i++) {
                     if (data[i] == "39") {
                         var productTypeId = 39;
->>>>>>> 39774622bbfac40ad4a2c5028ed46d645a7942f6
                     }
                 }
-                if (indTypeId == 7) {
-                    $("#display_result_control_other").removeClass("d-none");
-                    $("#result_control_other").prop("required", true);
+                if (productTypeId == 39) {
+                    $("#display_product_type_other").removeClass("d-none");
+                    $("#product_type_other").prop("required", true);
                 } else {
-                    $("#display_result_control_other").addClass("d-none");
-                    $("#result_control_other").prop("required", false);
-                    $("#result_control_other").val(null);
+                    $("#display_product_type_other").addClass("d-none");
+                    $("#product_type_other").prop("required", false);
+                    $("#product_type_other").val(null);
                 }
             }
-<<<<<<< HEAD
-        );
-    }
-    checkResultControl();
-
-    //4.7
-    function checkTestCalibrate() {
-        // this value
-        var data = $("#testing_calibrating_type_id").val();
-        if (data != null) {
-            for (i = 0; i < data.length; i++) {
-                if (data[i] == "6") {
-                    var indTypeId = 6;
-                }
-=======
             if (productTypeId == 39) {
                 $("#display_product_type_other").removeClass("d-none");
                 $("#product_type_other").prop("required", true);
@@ -150,56 +67,13 @@ $(document).ready(function() {
                 $("#display_product_type_other").addClass("d-none");
                 $("#product_type_other").prop("required", false);
                 $("#product_type_other").val(null);
->>>>>>> 39774622bbfac40ad4a2c5028ed46d645a7942f6
             }
-        }
-        if (indTypeId == 6) {
-            $("#display_testing_calibrating_type_other").removeClass("d-none");
-            $("#testing_calibrating_type_other").prop("required", true);
-        } else {
-            $("#display_testing_calibrating_type_other").addClass("d-none");
-            $("#testing_calibrating_type_other").prop("required", false);
-            $("#testing_calibrating_type_other").val(null);
-        }
-        // event on selected
-        $("#testing_calibrating_type_id").on(
-            "select2:select select2:unselect",
-            function() {
-                var data = $("#testing_calibrating_type_id").val();
-                if (data != null) {
-                    for (i = 0; i < data.length; i++) {
-                        if (data[i] == "6") {
-                            var indTypeId = 6;
-                        }
-                    }
-                }
-                if (indTypeId == 6) {
-                    $("#display_testing_calibrating_type_other").removeClass(
-                        "d-none"
-                    );
-                    $("#testing_calibrating_type_other").prop("required", true);
-                } else {
-                    $("#display_testing_calibrating_type_other").addClass(
-                        "d-none"
-                    );
-                    $("#testing_calibrating_type_other").prop(
-                        "required",
-                        false
-                    );
-                    $("#testing_calibrating_type_other").val(null);
-                }
-            }
-        );
+        });
     }
-    checkTestCalibrate();
+    checkProductType();
 
-<<<<<<< HEAD
-    //4.14 checkTestingCalibratingType
-    function checkTestType() {
-=======
     //4.7 testingCalibratingTypeId
     function checkTestingCalibratingTypeId() {
->>>>>>> 39774622bbfac40ad4a2c5028ed46d645a7942f6
         // this value
         var data = $("#testing_calibrating_type_id").val();
         if (data == "6") {
@@ -361,65 +235,5 @@ $(document).ready(function() {
             }
         );
     }
-<<<<<<< HEAD
-    checkPT();
-
-    function getEquipmentLab() {
-        var oldlabID = $("#lab_id").attr("data-value");
-        // var oldEquipID = document.getElementById("equipments_id").innerHTML;
-        var oldEquipID = $("#equipments_id").attr("data-value");
-        // var oldEquipID = $("#equipments_id").val([1, 2, 3]);
-        // console.log(oldlabID);
-        // console.log(oldEquipID);
-
-        // เพื่อให้ค่าเก่าของ Lab ID กลับมาด้วย
-        if (oldlabID != "") {
-            $.ajax({
-                url: "/productLab/equipmentinLab/" + oldlabID,
-                type: "GET",
-                dataType: "JSON",
-                success: function(data) {
-                    // console.log(data);
-                    $.each(data, function(key, value) {
-                        $("#equipments_id").append(`
-                                <option value="${value.id}">${value.equipment_code}</option>
-                            `);
-                    });
-                }
-            });
-        } else {
-            $("#equipments_id").html(`<option value=""></option>`);
-            $("#equipments_id").trigger("change");
-        }
-
-        // เลือก Lab ID จากนั้นแสดงรายการเครื่องมือของแลปนั้นๆ
-        $("#lab_id").on("select2:select select2:unselect", function() {
-            var id_lab = $("#lab_id").val();
-            $("#equipments_id").html(`<option value=""></option>`);
-            $("#equipments_id").trigger("change");
-            console.log(id_lab);
-            if (id_lab != "") {
-                $.ajax({
-                    url: "/productLab/equipmentinLab/" + id_lab,
-                    type: "GET",
-                    dataType: "JSON",
-                    success: function(data) {
-                        console.log(data);
-                        $.each(data, function(key, value) {
-                            $("#equipments_id").append(`
-                                <option value=" ${value.id} " {{ in_array(${value.id}, old('equipments_id') ? : []) ? 'selected' : '' }}> ${value.equipment_code} </option>
-                            `);
-                        });
-                    }
-                });
-            } else {
-                $("#equipments_id").html(`<option value=""></option>`);
-                $("#equipments_id").trigger("change");
-            }
-        });
-    }
-    getEquipmentLab();
-=======
     checkProficiencyTestingId();
->>>>>>> 39774622bbfac40ad4a2c5028ed46d645a7942f6
 });
